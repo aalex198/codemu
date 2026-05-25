9f85cebd56ee42e3a77a82de8b77ce0fe488ca09
aalex98 <ipalexeev98@mail.ru>
2026-05-25 09:27:00 +0300

    refactor: add prettier

--- js/1/1.js
+++ js/1/1.js
@@ -1,7 +1,7 @@
 // №1 Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
 function isNegative(num) {
-  if (typeof num !== 'number' || Number.isNaN(num)) {
-    throw new TypeError('Параметр должен быть числом');
+  if (typeof num !== "number" || Number.isNaN(num)) {
+    throw new TypeError("Параметр должен быть числом");
   }
 
   return num < 0;
@@ -9,21 +9,21 @@ function isNegative(num) {
 
 // №2 Дана строка. Выведите в консоль длину этой строки.
 function stringLength(str) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Параметр должен быть строкой');
-  }
-
-  return str.length;
-};
-
-// №3 Дана строка. Выведите в консоль последний символ строки.
-function getLastLetterFromString(str) {
-  if (typeof str !== 'string') {
+  if (typeof str !== "string") {
     throw new TypeError("Параметр должен быть строкой");
   }
 
-  if(str.trim().length < 1) {
-    throw new Error("Передана пустая строка")
+  return str.length;
+}
+
+// №3 Дана строка. Выведите в консоль последний символ строки.
+function getLastLetterFromString(str) {
+  if (typeof str !== "string") {
+    throw new TypeError("Параметр должен быть строкой");
+  }
+
+  if (str.trim().length < 1) {
+    throw new Error("Передана пустая строка");
   }
 
   return str[str.length - 1];
@@ -31,18 +31,17 @@ function getLastLetterFromString(str) {
 
 // №4 Дано число. Проверьте, четное оно или нет.
 function isEvenNumber(num) {
-  if (typeof num !== "number" || !Number.isFinite(num) ) {
-    throw new TypeError('Параметр должен быть числом!');
+  if (typeof num !== "number" || !Number.isFinite(num)) {
+    throw new TypeError("Параметр должен быть числом!");
   }
 
   return num % 2 === 0;
 }
 
-
 // №5 Даны два слова. Проверьте, что первые буквы этих слов совпадают.
 function isEqualLetterOfTwoWords(twoWords) {
   if (typeof twoWords !== "string") {
-    throw new TypeError("Параметр должен быть строкой"); 
+    throw new TypeError("Параметр должен быть строкой");
   }
   const trimmed = twoWords.trim();
 
@@ -59,9 +58,9 @@ function isEqualLetterOfTwoWords(twoWords) {
   return words[0][0].toLowerCase() === words[1][0].toLowerCase();
 }
 
-module.exports = { 
-  isNegative, 
-  stringLength, 
+module.exports = {
+  isNegative,
+  stringLength,
   getLastLetterFromString,
   isEvenNumber,
   isEqualLetterOfTwoWords,
