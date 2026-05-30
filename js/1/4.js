f05559f38215b84a0ba1fe85cb766428b7258193
aalex98 <ipalexeev98@mail.ru>
2026-05-30 21:27:46 +0300

    feat: 4.3 task

--- js/1/4.js
+++ js/1/4.js
@@ -15,9 +15,16 @@ function fromNegativeOneHundredToZero() {
 }
 
 // №3 Выведите в консоль все целые числа от 100 до 1.
-
+function logsDigitsFromOneHangredToOne() {
+  let i = 100;
+  do {
+    console.log(i);
+    i--;
+  } while (i >= 1);
+}
 // №4 Выведите в консоль все четные числа из промежутка от 1 до 100.
+// рекурсией
 
 // №5 Выведите в консоль все числа кратные трем в промежутке от 1 до 100.
 
-module.exports = { logsOneHundred, fromNegativeOneHundredToZero };
+module.exports = { logsOneHundred, fromNegativeOneHundredToZero, logsDigitsFromOneHangredToOne };
--- js/1/4.test.js
+++ js/1/4.test.js
@@ -1,4 +1,8 @@
-const { logsOneHundred, fromNegativeOneHundredToZero } = require("./4.js");
+const {
+  logsOneHundred,
+  fromNegativeOneHundredToZero,
+  logsDigitsFromOneHangredToOne,
+} = require("./4.js");
 
 describe("logsOneHundred", () => {
   let consoleSpy;
@@ -57,6 +61,38 @@ describe("fromNegativeOneHundredToZero", () => {
 
     for (let i = 0; i < 101; i++) {
       const expectedValue = -100 + i;
+      expect(allCalls[i][0]).toBe(expectedValue);
+    }
+  });
+});
+
+describe("logsDigitsFromOneHangredToOne", () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
+  test("выводит числа от 100 до 1", () => {
+    logsDigitsFromOneHangredToOne();
+
+    expect(consoleSpy).toHaveBeenCalledTimes(100);
+    expect(consoleSpy).toHaveBeenNthCalledWith(1, 100);
+    expect(consoleSpy).toHaveBeenNthCalledWith(100, 1);
+    expect(consoleSpy).toHaveBeenNthCalledWith(50, 51);
+  });
+
+  test("выводит корректную последовательность", () => {
+    logsDigitsFromOneHangredToOne();
+
+    const allCalls = consoleSpy.mock.calls;
+
+    for (let i = 0; i < 100; i++) {
+      const expectedValue = 100 - i;
       expect(allCalls[i][0]).toBe(expectedValue);
     }
   });
