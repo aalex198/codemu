327c90717c13b98865137ed374c243992bc0cad4
aalex98 <ipalexeev98@mail.ru>
2026-06-18 12:18:35 +0300

    feat: task 1.9.3

--- js/1/9.js
+++ js/1/9.js
@@ -4,10 +4,16 @@ function filterUrls(urls, startParam = "http://") {
     throw new TypeError("Аргумент должен быть массивом строк");
   }
 
+  if (typeof startParam !== "string") {
+    throw new TypeError("Второй параметр должен быть строкой");
+  }
+
   return urls.filter(url => {
     if (typeof url !== "string") {
       throw new TypeError("Массив должен состоять из строк!");
     }
+
+    if (startParam === "") return false;
     return url.startsWith(startParam);
   });
 }
@@ -18,15 +24,49 @@ function filterFilesOnExtensions(extensions, findExtension = ".html") {
     throw new TypeError("Аргумент должен быть массивом");
   }
 
+  if (typeof findExtension !== "string") {
+    throw new TypeError("Второй параметр должен быть строкой");
+  }
+
   return extensions.filter(extension => {
     if (typeof extension !== "string") {
       throw new TypeError("Массив должен состоять только из строк");
     }
+
+    if (findExtension === "") return false;
 
     return extension.endsWith(findExtension);
   });
 }
 
 // №3 Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} percent с диапазоном от 0 до 100
+ */
+function increaseByPercent(nums, percent = 10) {
+  if (!Array.isArray(nums)) {
+    throw new TypeError("Аргумент должен быть массивом");
+  }
 
-module.exports = { filterUrls, filterFilesOnExtensions };
+  if (typeof percent !== "number") {
+    throw new TypeError("Второй параметр должен быть числом");
+  }
+
+  if (percent < 0 || percent > 100) {
+    throw new Error(
+      "Ошибка диапазона! Число (значение коэффициента) не может быть меньше нуля или больше ста процентов"
+    );
+  }
+
+  return nums.map(num => {
+    if (typeof num !== "number" || !Number.isFinite(num)) {
+      throw new TypeError("Аргумент массива должен быть числом");
+    }
+
+    return num + (num / 100) * percent;
+  });
+}
+
+module.exports = { filterUrls, filterFilesOnExtensions, increaseByPercent };
--- js/1/9.test.js
+++ js/1/9.test.js
@@ -1,10 +1,11 @@
-const { filterUrls, filterFilesOnExtensions } = require("./9.js");
+const { filterUrls, filterFilesOnExtensions, increaseByPercent } = require("./9.js");
 
 describe("filterUrls", () => {
   test("Строки", () => {
     expect(filterUrls([])).toEqual([]);
     expect(filterUrls(["abc", "http://google.com"])).toEqual(["http://google.com"]);
     expect(filterUrls(["http://google.com"])).toEqual(["http://google.com"]);
+    expect(filterUrls(["http://google.com"], "")).toEqual([]);
     expect(filterUrls(["http://google.com", "http://ya.ru"])).toEqual([
       "http://google.com",
       "http://ya.ru",
@@ -21,7 +22,13 @@ describe("filterUrls", () => {
     );
   });
 
-  test("не число в аргументе — ошибка", () => {
+  test("В аргемент подстроки передана не строка — ошибка", () => {
+    expect(() => filterUrls(["http://google.com", "dsfds"], 22)).toThrow(
+      TypeError("Второй параметр должен быть строкой")
+    );
+  });
+
+  test("не массив в аргументе — ошибка", () => {
     expect(() => filterUrls("123")).toThrow(TypeError("Аргумент должен быть массивом строк"));
     expect(() => filterUrls(1, "456")).toThrow(TypeError("Аргумент должен быть массивом строк"));
     expect(() => filterUrls()).toThrow(TypeError("Аргумент должен быть массивом строк"));
@@ -42,11 +49,15 @@ describe("filterFilesOnExtensions", () => {
     expect(filterFilesOnExtensions([])).toEqual([]);
     expect(filterFilesOnExtensions(["index.pug"])).toEqual([]);
     expect(filterFilesOnExtensions(["abc.html", "http://google.com"])).toEqual(["abc.html"]);
+    expect(filterFilesOnExtensions(["abc.html", "http://google.com"], "")).toEqual([]);
     expect(filterFilesOnExtensions(["index.html"])).toEqual(["index.html"]);
     expect(filterFilesOnExtensions(["page.html", "main.html"])).toEqual(["page.html", "main.html"]);
     expect(filterFilesOnExtensions(["index.html", "index.hbs", "profile.html"])).toEqual([
       "index.html",
       "profile.html",
+    ]);
+    expect(filterFilesOnExtensions(["index.html", "index.hbs", "profile.html"], ".hbs")).toEqual([
+      "index.hbs",
     ]);
   });
 
@@ -56,7 +67,13 @@ describe("filterFilesOnExtensions", () => {
     );
   });
 
-  test("не число в аргументе — ошибка", () => {
+  test("В аргемент подстроки передана не строка — ошибка", () => {
+    expect(() => filterUrls(["index.html", "profile.html"], 22)).toThrow(
+      TypeError("Второй параметр должен быть строкой")
+    );
+  });
+
+  test("не массив в аргументе — ошибка", () => {
     expect(() => filterFilesOnExtensions("123")).toThrow(
       TypeError("Аргумент должен быть массивом")
     );
@@ -81,5 +98,49 @@ describe("filterFilesOnExtensions", () => {
     );
     expect(() => filterFilesOnExtensions(true)).toThrow(TypeError("Аргумент должен быть массивом"));
     expect(() => filterFilesOnExtensions("")).toThrow(TypeError("Аргумент должен быть массивом"));
+  });
+});
+
+describe("increaseByPercent", () => {
+  test("Числа", () => {
+    expect(increaseByPercent([1, 2, 3])).toEqual([1.1, 2.2, 3.3]);
+  });
+
+  test("в массиве не число — ошибка", () => {
+    expect(() => increaseByPercent([1, "index.html", 3])).toThrow(
+      TypeError("Аргумент массива должен быть числом")
+    );
+  });
+  test("в массиве не число — ошибка", () => {
+    expect(() => increaseByPercent([1, 3], "22")).toThrow(
+      TypeError("Второй параметр должен быть числом")
+    );
+  });
+  test("Второй параметр вне допустимого процентного диапазона — ошибка", () => {
+    expect(() => increaseByPercent([1, 3], 120)).toThrow(
+      Error(
+        "Ошибка диапазона! Число (значение коэффициента) не может быть меньше нуля или больше ста процентов"
+      )
+    );
+    expect(() => increaseByPercent([1, 3], -1)).toThrow(
+      Error(
+        "Ошибка диапазона! Число (значение коэффициента) не может быть меньше нуля или больше ста процентов"
+      )
+    );
+  });
+
+  test("не массив в аргументе — ошибка", () => {
+    expect(() => increaseByPercent("123")).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(1, "456")).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent()).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(null)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(1, null)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(NaN)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(NaN, 1)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(Infinity)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent({})).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(false)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent(true)).toThrow(TypeError("Аргумент должен быть массивом"));
+    expect(() => increaseByPercent("")).toThrow(TypeError("Аргумент должен быть массивом"));
   });
 });
